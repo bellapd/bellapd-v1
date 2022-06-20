e3a2c256df27a155b1a030f2a9b04bd99d42d8df
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-20 22:06:59 +0700

    feat: optimize and refactor

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,15 +1,37 @@
-import type { NextPage } from 'next'
-import Layout from '../components/layouts/Page'
-import { Heading, Stack } from '@chakra-ui/react'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Layout from '../components/Layout'
+import Posts from './../components/Posts'
+import { Heading } from '@chakra-ui/react'
+import type { IPost } from '../types/post.type'
 
-const Portfolio: NextPage = () => {
+export default function Portfolio({ portfolios }: { portfolios: IPost[] }): JSX.Element {
     return (
         <Layout>
-            <Stack align="center">
-                <Heading>This is Portfolio page</Heading>
-            </Stack>
+            <Heading as="h2" size="md" mb={3}>Portfolio</Heading>
+            <Posts posts={portfolios} />
         </Layout>
     )
 }
 
-export default Portfolio
+export const getStaticProps = async () => {
+    // get all folders' in content/portfolios
+    const folders = fs.readdirSync(path.join(process.cwd(), 'content', 'portfolios'))
+    
+    // iterate through all the folders
+    const portfolios = folders.map(slug => {
+        const content = fs.readFileSync(path.join('content', 'portfolios', slug, 'index.mdx'), 'utf-8')
+        const { data: frontMatter } = matter(content)
+        return {
+            frontMatter,
+            slug: slug
+        }
+    })
+
+    return {
+        props: {
+            portfolios
+        }
+    }
+}
