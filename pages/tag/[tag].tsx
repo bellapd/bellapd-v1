8708d11518ae32d6c6a42ae2143fa9cab662f24e
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-21 22:55:07 +0700

    style: decrease or remove margin size

---
+++ pages/tag/[tag].tsx
@@ -0,0 +1,76 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Layout from '../../components/Layout'
+import { Flex, Heading, Text } from '@chakra-ui/react'
+import Posts from '../../components/Posts'
+import type { IPost } from '../../types/post.type'
+
+function Tag({ posts, tag }: { posts: IPost[], tag: string }) {
+    return (
+        <Layout>
+            <Heading as="h1" size="xl">
+                <Flex>
+                    {posts.length} posts tagged with
+                    <Text
+                        as="p"
+                        // pick a color from the "colors" array
+                        color='tagColor'
+                    >&nbsp;#</Text><Text as="p">{tag}</Text>
+                </Flex>
+            </Heading>
+            <Posts posts={posts}/>
+        </Layout>
+    )
+}
+
+const getStaticPaths = async () => {
+    const folders = fs.readdirSync(path.join('content', 'blogs'))
+
+    const obj: Record<string, boolean> = {}
+
+    // get all tags from all posts
+    folders.forEach(slug => {
+        const content = fs.readFileSync(path.join('content', 'blogs', slug, 'index.mdx'), 'utf8')
+        const { data: frontMatter } = matter(content)
+        frontMatter.tags.forEach(
+            // tag is string type
+            (tag: string) => obj[`/tag/${tag}`] = true
+        )
+    })
+
+    return {
+        paths: Object.keys(obj),
+        fallback: false
+    }
+}
+
+const getStaticProps = async ({ params: { tag } }: any) => {
+    // get all folders' in content/portfolios
+    const folders = fs.readdirSync(path.join('content', 'blogs'))
+
+    // get all posts' front matter with a specific tag
+    const filtered = folders.map(slug => {
+        const content = fs.readFileSync(path.join('content', 'blogs', slug, 'index.mdx'), 'utf-8')
+        const { data: frontMatter } = matter(content)
+        if (frontMatter.tags.includes(tag)) {
+            return {
+                frontMatter,
+                slug: slug
+            }
+        }
+    })
+
+    // remove undefined elements in the "posts" array
+    const posts = filtered.filter(Boolean)
+
+    return {
+        props: {
+            posts,
+            tag
+        }
+    }
+}
+
+export { getStaticProps, getStaticPaths }
+export default Tag
