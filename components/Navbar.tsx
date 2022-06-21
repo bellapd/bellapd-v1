f02ea2d1aa038cfa8dc284bb5fff3d6e45788d19
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-21 22:57:44 +0700

    fix: make marginLeft and change home directory

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,105 +1,91 @@
 import {
-    Avatar,
-    Box,
-    Button,
-    Center,
-    Flex,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuDivider,
-    Text,
-
-} from '@chakra-ui/react'
-import Navlink from './Navlink'
-import Link from 'next/link'
-import styled from '@emotion/styled'
-import Switch from './Switch'
-import Accent from './Accent'
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Text,
+} from "@chakra-ui/react";
+import Navlink from "./Navlink";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import Switch from "./Switch";
 
 export default function Navigation(): JSX.Element {
-    const Navbar = styled(Box)`
-        position: sticky;
-        z-index: 10;
-        top: 0;
-        backdrop-filter: saturate(180%) blur(20px);
-        transition: height .5s, line-height .5s;
-    `
+  const Navbar = styled(Box)`
+    position: sticky;
+    z-index: 10;
+    top: 0;
+    backdrop-filter: saturate(100%) blur(20px);
+    transition: height 0.5s, line-height 0.5s;
+  `;
 
-    return (
-        <Navbar
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            height="100%"
-            width="100%"
-            as="nav"
-        >
-            <Flex
-                py={2}
-                px={5}
-                maxW="container.sm"
-                align="center"
-                mx="auto"
+  return (
+    <Navbar
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      height="100%"
+      width="100%"
+      as="nav"
+    >
+      <Flex py={2} px={5} maxW="container.sm" align="center" mx="auto">
+        <Flex marginLeft="auto">
+          <Flex display={{ base: "none", md: "flex" }}>
+            <Navlink href="/">Home</Navlink>
+            <Navlink href="/about">About</Navlink>
+            <Navlink href="/portfolio">Portfolio</Navlink>
+          </Flex>
+          {/* <Accent /> */}
+          <Switch />
+          <Menu>
+            <MenuButton
+              // show menu only when container is smaller than 768px
+              display={{ base: "none", md: "none", sm: "flex" }}
+              as={Button}
+              // rounded={'full'}
+              variant={"link"}
+              cursor={"pointer"}
+              minW={0}
             >
-                <Flex>
-                    <Link href="/" passHref>
-                        Logo
-                    </Link>
-                </Flex>
-                <Flex marginLeft="auto">
-                    <Flex
-                        display={{ base: 'none', md: 'flex' }}
-                    >
-                        <Navlink href="/about">About</Navlink>
-                        <Navlink href="/portfolio">Portfolio</Navlink>
-                    </Flex>
-                    <Accent />
-                    <Switch />
-                    <Menu>
-                        <MenuButton
-                            // show menu only when container is smaller than 768px
-                            display={{ base: 'none', md: 'none', sm: 'flex' }}
-                            as={Button}
-                            rounded={'full'}
-                            variant={'link'}
-                            cursor={'pointer'}
-                            minW={0}
-                        >
-                        <Avatar
-                            size={'sm'}
-                            src={'https://avatars.dicebear.com/api/male/username.svg'}
-                        />
-                        </MenuButton>
-                        <MenuList alignItems={'center'}>
-                        <br />
-                        <Center>
-                            <Avatar
-                                size={'2xl'}
-                                src={'https://avatars.dicebear.com/api/male/username.svg'}
-                            />
-                        </Center>
-                        <br />
-                        <Center>
-                            <Text as="p">Bijon Setyawan Raya</Text>
-                        </Center>
-                        <br />
-                        <MenuDivider />
-                        <MenuItem>
-                            <Link href="/about">
-                                About
-                            </Link>
-                        </MenuItem>
-                        <MenuItem>
-                            <Link href="/portfolio">
-                                Portfolio
-                            </Link>
-                        </MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Flex>
-            </Flex>
-        </Navbar>
-    )
+              <Avatar
+                size={"sm"}
+                src={
+                  "https://avatars.dicebear.com/api/adventurer-neutral/:seed.svg"
+                }
+              />
+            </MenuButton>
+            <MenuList alignItems={"center"}>
+              <br />
+              <Center>
+                <Avatar
+                  size={"2xl"}
+                  src={
+                    "https://avatars.dicebear.com/api/adventurer-neutral/:seed.svg"
+                  }
+                />
+              </Center>
+              <br />
+              <Center>
+                <Text as="p">Annabella Putri Dirgo</Text>
+              </Center>
+              <br />
+              <MenuDivider />
+              <MenuItem>
+                <Link href="/about">About</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/portfolio">Portfolio</Link>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+    </Navbar>
+  );
 }
