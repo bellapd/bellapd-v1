42c66ab51ed660b963f8dc46295dedf9dda7527e
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-25 00:35:08 +0800

    feat: responsive navbar menu with media query

---
+++ components/Dropdown.tsx
@@ -0,0 +1,47 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+    LinkBox,
+    LinkOverlay,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+export default function Dropdown(): JSX.Element {
+    return (
+        <Menu>
+            <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                isRound
+                variant="ghost"
+            />
+            <MenuList alignItems={"center"}>
+                <LinkBox>
+                <MenuItem>
+                    <NextLink href="/" passHref>
+                    <LinkOverlay>Home</LinkOverlay>
+                    </NextLink>
+                </MenuItem>
+                </LinkBox>
+                <LinkBox>
+                <MenuItem>
+                    <NextLink href="/about" passHref>
+                    <LinkOverlay>About</LinkOverlay>
+                    </NextLink>
+                </MenuItem>
+                </LinkBox>
+                <LinkBox>
+                <MenuItem>
+                    <NextLink href="/portfolio" passHref>
+                    <LinkOverlay>Portfolio</LinkOverlay>
+                    </NextLink>
+                </MenuItem>
+                </LinkBox>
+            </MenuList>
+        </Menu>
+    );
+}
