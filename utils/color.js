6a77aee6ce638193fcf964202e3ccf0cd26cb3d3
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-27 13:39:28 +0700

    feat: change color palette

--- utils/color.js
+++ utils/color.js
@@ -1,4 +1,4 @@
 export function Color() {
-  const colors = ["gray", "red", "orange", "green", "teal", "blue", "cyan"];
+  const colors = ["gray", "red", "orange", "blue", "yellow"];
   return colors[Math.floor(Math.random() * colors.length)];
 }
