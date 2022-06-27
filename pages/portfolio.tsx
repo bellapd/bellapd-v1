ec0f3812a82e4d2fe48360b7a45ade639074303b
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-27 22:22:12 +0700

    bug: cannot redirect into portfolio content

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -3,8 +3,7 @@ import path from "path";
 import matter from "gray-matter";
 import Layout from "../components/Layout";
 import Posts from "./../components/Posts";
-import { DateTime } from "luxon";
-import { Heading, Box, Stack } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import type { IPost } from "../types/post.type";
 
 export default function Portfolio({
@@ -14,25 +13,22 @@ export default function Portfolio({
 }): JSX.Element {
   return (
     <Layout>
-      <Stack align="left">
-        <Heading as="h1" size="3xl">
-          <Box as="span" bg="#24269B" bgClip="text">
-            P
-          </Box>
-          ortfolio
-        </Heading>
-      </Stack>
+      <Heading as="h2" size="md" mb={3}>
+        Portfolio
+      </Heading>
       <Posts posts={portfolios} />
     </Layout>
   );
 }
 
 export const getStaticProps = async () => {
+  // get all folders' in content/portfolios
   const folders = fs.readdirSync(
     path.join(process.cwd(), "content", "portfolios")
   );
 
-  var posts = folders.map((slug) => {
+  // iterate through all the folders
+  const portfolios = folders.map((slug) => {
     const content = fs.readFileSync(
       path.join("content", "portfolios", slug, "index.mdx"),
       "utf-8"
@@ -44,21 +40,9 @@ export const getStaticProps = async () => {
     };
   });
 
-  posts.sort((a, b) => {
-    const aDate: typeof a.frontMatter.date = DateTime.fromFormat(
-      a.frontMatter.date,
-      "LLLL dd, yyyy"
-    );
-    const bDate: typeof a.frontMatter.date = DateTime.fromFormat(
-      b.frontMatter.date,
-      "LLLL dd, yyyy"
-    );
-    return bDate - aDate;
-  });
-
   return {
     props: {
-      posts,
+      portfolios,
     },
   };
 };
