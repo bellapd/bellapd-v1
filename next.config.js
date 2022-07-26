84d0101688a2befd7e24be34d42757f106457eb9
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-07-26 13:00:41 +0700

    fix: add Webpack Bundle Analyzer to remove unused package

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
 /**
  * @type {import('next').NextConfig}
-*/
-const withPlugins = require('next-compose-plugins')
+ */
+const withPlugins = require("next-compose-plugins");
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
-    optimizeFonts: true,
-    ignoreBuildErrors: false,
-    reactStrictMode: true,
-    eslint: {
-        dirs: ['components', 'content', 'pages', 'types', 'utils']
-    },
-    compiler: {
-        emotion: true,
-    },
-}
+  optimizeFonts: true,
+  ignoreBuildErrors: false,
+  reactStrictMode: true,
+  eslint: {
+    dirs: ["components", "content", "pages", "types", "utils"],
+  },
+  compiler: {
+    emotion: true,
+  },
+};
 
-const runtimeCaching = require('next-pwa/cache')
-const withPWA = require('next-pwa')({
-    pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development',
-        runtimeCaching,
-    }
-})
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
+});
 
-module.exports = withPlugins([
-    nextConfig,
-    withPWA,
-])
+module.exports = withPlugins([nextConfig, [withBundleAnalyzer], withPWA]);
