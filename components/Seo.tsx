4aad83fe0063b4f01e622cee0a65cb12ec51fef0
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-07-26 11:54:13 +0700

    chore: fix SEO

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,14 +1,67 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-const Seo = () => {
-    return (
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content="Bella's homepage" />
-            <meta name="author" content="Annabella Putri Dirgo" />
-            <title>Annabella Putri Dirgo - Homepage</title>
-        </Head>
-    )
+export default function Seo({
+  title,
+  publish,
+  description,
+  type,
+}: {
+  title: string;
+  publish?: string;
+  description?: string;
+  type: string;
+}): JSX.Element {
+  const router = useRouter();
+  const meta = {
+    author: "Annabella Putri Dirgo",
+    description: "An Computer Science Undergraduate Student",
+    avatar: "https://bellapd.vercel.app/images/avatar.png",
+  };
+  return (
+    <Head>
+      <title>
+        {title} - {meta.author}
+      </title>
+      <meta name="title" content={title} />
+      <meta name="robots" content="follow, index" />
+      <meta name="viewport" content="width=device-width" />
+      <meta name="author" content={meta.author} />
+      <link
+        rel="canonical"
+        href={`https://bellapd.vercel.app${router.asPath}`}
+      />
+      <link rel="icon" href={meta.avatar} />
+
+      <meta property="og:type" content={type} />
+      <meta
+        property="og:url"
+        content={`https://bellapd.vercel.app${router.asPath}`}
+      />
+      <meta property="og:title" content={title + " - " + meta.author} />
+      <meta property="og:image" content={meta.avatar} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="@BijonRaya" />
+      <meta
+        property="twitter:creator"
+        content={`https://bellapd.vercel.app${router.asPath}`}
+      />
+      <meta property="twitter:image" content={meta.avatar} />
+      {!description ? (
+        <>
+          <meta name="description" content={meta.description} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="twitter:description" content={meta.description} />
+        </>
+      ) : (
+        <>
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta property="twitter:description" content={description} />
+        </>
+      )}
+      {publish && <meta property="article:published_time" content={publish} />}
+    </Head>
+  );
 }
-
-export default Seo
