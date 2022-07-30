a4fe225eb55e59a42720df0a7fb2a1f1c4a4b71d
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-07-30 14:03:59 +0700

    style: change heading

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,60 @@
-import { Box, BoxProps } from "@chakra-ui/react";
-import { PropsWithChildren } from "react";
+import * as React from "react";
+import {
+  Container,
+  chakra,
+  Stack,
+  Text,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-interface HeaderProps extends BoxProps {
-  underlineColor: string;
-}
+const HeroSection = () => {
+  return (
+    <Container p={{ base: 2, sm: 7 }}>
+      <Stack direction="column" spacing={6} alignItems="center">
+        <Box
+          py={2}
+          px={3}
+          bg="dark"
+          w="max-content"
+          color="white"
+          rounded="md"
+          fontSize="sm"
+          _hover={{
+            transform: "scale(1.1)",
+            transition: "all .25s ease-in-out",
+          }}
+        >
+          <Stack direction={{ base: "column", sm: "row" }}>
+            <Text fontWeight="bold">Welcome 👋🏼</Text>
+          </Stack>
+        </Box>
+        <chakra.h1
+          fontSize={{ base: "4xl", sm: "5xl" }}
+          fontWeight="bold"
+          textAlign="center"
+          maxW="600px"
+        >
+          My name is {""}
+          <chakra.span
+            color="dark"
+            bg="linear-gradient(transparent 50%, #F0C940 50%)"
+          >
+            Bella
+          </chakra.span>
+        </chakra.h1>
+        <Text
+          maxW="550px"
+          fontSize="xl"
+          textAlign="center"
+          color={useColorModeValue("black", "white")}
+        >
+          I'm a full time student and a long time Learner 👩🏻‍💻. I write about
+          things I'm working on and share what I've learned 🥸
+        </Text>
+      </Stack>
+    </Container>
+  );
+};
 
-const Header = ({
-  children,
-  underlineColor,
-  ...props
-}: PropsWithChildren<HeaderProps>) => (
-  <Box
-    as="h1"
-    mt={10}
-    mb={6}
-    fontSize="3xl"
-    lineHeight="shorter"
-    fontWeight="bold"
-    {...props}
-    textAlign="left"
-  >
-    <UnderlinedText color={underlineColor}>{children}</UnderlinedText>
-  </Box>
-);
-
-interface UnderlinedTextProps {
-  color: string;
-  h?: string;
-  zIndex?: number;
-}
-
-const UnderlinedText = (props: PropsWithChildren<UnderlinedTextProps>) => (
-  <Box as="span" display="inline-block" position="relative">
-    {props.children}
-    <Box
-      as="span"
-      display="block"
-      position="absolute"
-      bg={props.color || "gray.200"}
-      w="100%"
-      h={props.h || "1px"}
-      bottom={-2}
-    />
-  </Box>
-);
-
-export default Header;
+export default HeroSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,119 +4,16 @@ import matter from "gray-matter";
 import Posts from "../components/Posts";
 import Layout from "../components/Layout";
 import * as React from "react";
-import {
-  Flex,
-  Avatar,
-  Box,
-  Container,
-  Stack,
-  Text,
-  useMediaQuery,
-} from "@chakra-ui/react";
-import { MotionBox, MotionFlex } from "../components/Motion";
+import { Stack, Text } from "@chakra-ui/react";
 import Header from "../components/Header";
 import type { IPost } from "../types/post.type";
 import { DateTime } from "luxon";
 import Readall from "../components/Readall";
 
-const ANIMATION_DURATION = 0.5;
-
 export default function Home({ posts }: { posts: IPost[] }): JSX.Element {
-  const color = "pink.400";
-
   return (
     <Layout>
-      <Container maxW="2xl">
-        <Flex
-          p={{ base: 2, md: 13 }}
-          w="100%"
-          direction={{ base: "column", md: "row" }}
-        >
-          <MotionBox
-            opacity="0"
-            initial={{
-              translateX: -150,
-              opacity: 0,
-            }}
-            animate={{
-              translateX: 0,
-              opacity: 1,
-              transition: {
-                duration: ANIMATION_DURATION,
-              },
-            }}
-            m="auto"
-            mb={[16, 16, "auto"]}
-          >
-            <MotionBox whileHover={{ scale: 1.2 }} rounded="full" shadow="lg">
-              <Avatar
-                size="2xl"
-                showBorder={true}
-                borderColor={color}
-                src={"/images/avatar.png"}
-              />
-            </MotionBox>
-          </MotionBox>
-          <MotionFlex
-            position="relative"
-            ml={["auto", "auto", 16]}
-            m={["auto", "initial"]}
-            w={["90%", "85%", "80%"]}
-            maxW="500px"
-            opacity="0"
-            justify="center"
-            direction="column"
-            initial={{
-              opacity: 0,
-              translateX: 150,
-            }}
-            animate={{
-              opacity: 1,
-              translateX: 0,
-              transition: {
-                duration: ANIMATION_DURATION,
-              },
-            }}
-          >
-            <Box position="relative">
-              <MotionBox whileHover={{ translateY: -5 }} width="max-content">
-                <Header
-                  underlineColor={color}
-                  mt={0}
-                  cursor="pointer"
-                  width="max-content"
-                >
-                  Welcome!
-                </Header>
-              </MotionBox>
-            </Box>
-            <Box as="h2" fontSize="2xl" fontWeight="400" textAlign="left">
-              My name is{" "}
-              <Box as="strong" fontWeight="600">
-                Bella
-              </Box>{" "}
-              and I&apos;m a{" "}
-              <Box as="span" whiteSpace="nowrap">
-                Full Time Student and
-              </Box>{" "}
-              <Box as="span" whiteSpace="nowrap">
-                an Long Time Learner 👩🏻‍💻
-              </Box>
-            </Box>
-            <Box
-              as="h2"
-              fontSize="2xl"
-              fontWeight="400"
-              mt={5}
-              textAlign="left"
-            >
-              I write about the things I&apos;m working on and share what
-              I&apos;ve learned 🥸
-            </Box>
-          </MotionFlex>
-        </Flex>
-      </Container>
-
+      <Header />
       <Stack align={"left"} pt={5}>
         <Text as="b" fontSize="xl" pb={2}>
           Recent posts
